efa274cfdcd95dbd7a5ef335658851244d85ca11
IORD1 <pratham111ingole@gmail.com>
2022-09-18 17:56:28 +0530

    added quote to main page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 window.onload = function() {
     func();
 };
@@ -15,9 +16,6 @@ function func(){
 
     var theme = '#' + Math.floor(Math.random() * 16777215).toString(16);
     document.getElementById("App").style.backgroundColor = theme;   
-    document.getElementById("new-quote").style.backgroundColor= theme;
-    document.getElementById("tweet-quote").style.backgroundColor= theme;
-    document.getElementById("tumblr-button").style.backgroundColor= theme;
     document.getElementById("quotes-on-quotes").style.color = theme;
     document.getElementById("text").style.color = theme;
     document.getElementById("author").style.color = theme;
